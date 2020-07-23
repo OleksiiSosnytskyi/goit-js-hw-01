6fb77f610282ebfd2a3f41d29df1a9f0c03bb36e
Oleksii <aleksey.sosnytskyi.jr@gmail.com>
2020-07-23 20:49:14 +0300

    переименовывает файлы

--- task-02.js
+++
@@ -1,8 +0,0 @@
-const total = 100;
-const ordered = 50;
-
-if (total < ordered) {
-  console.log('На складе недостаточно товаров!');
-} else {
-  console.log('Заказ оформлен, с вами свяжется менеджер');
-}
